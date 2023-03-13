17b4e8fcb7dd1ce141391096ee96b67c9ea210eb
Himanshu verma <121441629+himanshu123344@users.noreply.github.com>
2023-03-13 20:42:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+// images = 4 (0, 1, 2, 3)
+
+let flag = 0;
+
+function controller(x) {
+    flag = flag + x;
+    slideshow(flag);
+    //  left
+    // flag = 2 + (-1) = 1
+
+    // right
+    // flag = 2 + 1 = 3
+}
+
+slideshow(flag);
+
+function slideshow(num) {
+    let slides = document.getElementsByClassName('slide');
+
+    if (num == slides.length) {
+        flag = 0;
+        num = 0
+    }
+
+    for (let y of slides) {
+        y.style.display = "none"
+    }
+
+    slides[num].style.display = "block";
+}
